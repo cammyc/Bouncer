b947885e91f8b8f2b1c298ed45ef20c954cfaad8
cammyc <camconnor0903@gmail.com>
2017-04-22 16:18:06 -0600

    Event Slide Down

--- formHandlers/createEventForm.php
+++ formHandlers/createEventForm.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 $event_name = (empty($_POST["event_name"])) ? "" : $_POST["event_name"];
 $event_date = (empty($_POST["event_date"])) ? "" : $_POST["event_date"];
 $event_time = (empty($_POST["event_time"])) ? "" : $_POST["event_time"];
@@ -9,13 +11,13 @@ $event_image = (empty($_POST["event_image"])) ? "" : $_POST["event_image"];
 $event_type = (empty($_POST["event_type"])) ? "" : $_POST["event_type"];
 $cover_charge = (empty($_POST["cover_charge"])) ? "" : $_POST["cover_charge"];
 
-// echo "$event_name";
-// echo "$event_date";
-// echo "$event_time";
-// echo "$event_streetaddress";
-// echo "$event_description";
-// echo "$event_image";
-// echo "$event_type";
-// echo "$cover_charge";
+echo "$event_name";
+echo "$event_date";
+echo "$event_time";
+echo "$event_streetaddress";
+echo "$event_description";
+echo "$event_image";
+echo "$event_type";
+echo "$cover_charge";
 
 ?>
